0b66e8e694a13047f18552f9b57a89ad221d3eb2
Anastasia <Nastya170204@yandex.ru>
2023-09-11 11:07:55 +0700

    add comments

--- Task_1_1_1/src/main/java/Heapsort.java
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -4,10 +4,10 @@
 
 public class Heapsort {
     static void sift(int[] arr, int i, int n) {
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
+        int l = 2 * i + 1; //index of the left child
+        int r = 2 * i + 2; //index of the right child
         int t;
-        int largest = i;
+        int largest = i; //index of the largest element from a node of a parent and two children
         while (l < n) {
             if (arr[l] > arr[i]) {
                 largest = l;
@@ -15,7 +15,7 @@ public class Heapsort {
             if ((r < n) && (arr[r] > arr[i]) && (arr[r] > arr[l])) {
                 largest = r;
             }
-            if (largest != i) {
+            if (largest != i) { //the largest element appears at the root of the current subtree
                 t = arr[i];
                 arr[i] = arr[largest];
                 arr[largest] = t;
@@ -32,14 +32,14 @@ public class Heapsort {
     static void heap_sort(int[] arr) {
         int n = arr.length;
         int t;
-        for (int i = n / 2; i >= 0; i--) {
+        for (int i = n / 2; i >= 0; i--) { //making a heap
             sift(arr, i, n);
         }
-        for (int i = n - 1; i > 0; i--) {
+        for (int i = n - 1; i > 0; i--) { //sort the heap
             t = arr[0];
             arr[0] = arr[i];
             arr[i] = t;
-            sift(arr, 0, i);
+            sift(arr, 0, i); //the sorted sequence is formed at the tail of the array
         }
     }
 }
