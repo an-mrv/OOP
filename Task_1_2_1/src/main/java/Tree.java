fa09cf2fb821fa897d80ff51a62d3bc4432c9b3f
Anastasia Meshcheryakova <108881820+an-mrv@users.noreply.github.com>
2023-10-12 11:25:37 +0700

    Task_1_2_1 (#3)
    * Task_1_2_1
    
    * change code style
    
    * change style
    
    * Change style

---
+++ Task_1_2_1/src/main/java/Tree.java
@@ -0,0 +1,237 @@
+import java.util.*;
+
+/**
+ * Сlass for working with trees.
+ *
+ * @param <T> type for vertex values
+ */
+public class Tree<T> implements Iterable<T> {
+    private T data;
+    private Tree<T> parent;
+    private ArrayList<Tree<T>> children;
+    private int modCount = 0; //modification count
+
+    public Tree(T data) {
+        this.data = data;
+        this.children = new ArrayList<Tree<T>>();
+        this.parent = null;
+    }
+
+    private void changeModCountForAncestors() {
+        Tree<T> curParent = this.parent;
+        while (curParent != null) {
+            curParent.modCount++;
+            curParent = curParent.parent;
+        }
+    }
+
+    /**
+     * Add the value of a new child.
+     *
+     * @param data value for the new vertex
+     *
+     * @return child's subtree
+     */
+    public Tree<T> addChild(T data) {
+        this.modCount++;
+        this.changeModCountForAncestors();
+        Tree<T> newChild = new Tree<>(data);
+        this.children.add(newChild);
+        newChild.parent = this;
+        return newChild;
+    }
+
+    /**
+     * Add the subtree as a child.
+     *
+     * @param data subtree
+     */
+    public void addChild(Tree<T> data) {
+        this.modCount++;
+        this.changeModCountForAncestors();
+        this.children.add(data);
+        data.parent = this;
+    }
+
+    /**
+     * Deleting a subtree.
+     */
+    public void remove() {
+        this.modCount++;
+        this.changeModCountForAncestors();
+        if (this.parent != null) {
+            this.parent.children.remove(this);
+        }
+        this.children.clear();
+        this.data = null;
+    }
+
+    /**
+     * Checking for equality of two trees.
+     *
+     * @param obj tree
+     *
+     * @return is equal or not
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Tree)) {
+            return false;
+        }
+        if (((Tree<?>) obj).data != this.data) {
+            return false;
+        }
+        if (((Tree<?>) obj).children.size() != this.children.size()) {
+            return false;
+        }
+        for (int i = 0; i < this.children.size(); i++) {
+            if (!this.children.get(i).equals(((Tree<?>) obj).children.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Iterator for the BFS.
+     *
+     * @return BFS iterator
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new BfsIterator<>(this);
+    }
+
+    /**
+     * Class BfsIterator.
+     *
+     * @param <T> type for vertex values
+     */
+    class BfsIterator<T> implements Iterator<T> {
+        private int expectedModCount;
+        private Deque<Tree<T>> deque = new ArrayDeque<>();
+
+        BfsIterator(Tree<T> t) {
+            expectedModCount = t.modCount;
+            if (t.data != null) {
+                deque.addLast(t);
+            }
+        }
+
+        /**
+         * checking that there is the next element.
+         *
+         * @return has next element or not
+         */
+        @Override
+        public boolean hasNext() throws ConcurrentModificationException {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (this.deque.isEmpty()) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        /**
+         * returns the next element.
+         *
+         * @return next element
+         */
+        @Override
+        public T next() throws NoSuchElementException {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Tree<T> a = this.deque.pop();
+            for (int i = 0; i < a.children.size(); i++) {
+                if (a.children.get(i).data != null) {
+                    this.deque.addLast(a.children.get(i));
+                }
+            }
+            return a.data;
+        }
+
+        /**
+         * exception when calling remove.
+         */
+        @Override
+        public void remove() throws ConcurrentModificationException {
+            throw new ConcurrentModificationException();
+        }
+    }
+
+    /**
+     * Iterator for the DFS.
+     *
+     * @return DFS iterator
+     */
+    public Iterator<T> dfsiterator() {
+        return new DfsIterator<>(this);
+    }
+
+    /**
+     * Class DfsIterator.
+     *
+     * @param <T> type for vertex values
+     */
+    class DfsIterator<T> implements Iterator<T> {
+        private int expectedModCount;
+        private Deque<Tree<T>> stack = new ArrayDeque<>();
+        private Deque<Tree<T>> visited = new ArrayDeque<>();
+
+        DfsIterator(Tree<T> t) {
+            expectedModCount = t.modCount;
+            if (t.data != null) {
+                stack.addLast(t);
+            }
+        }
+
+        /**
+         * checking that there is the next element.
+         *
+         * @return has next element or not
+         */
+        @Override
+        public boolean hasNext() throws ConcurrentModificationException {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (this.stack.isEmpty()) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        /**
+         * returns the next element.
+         *
+         * @return next element
+         */
+        @Override
+        public T next() throws NoSuchElementException {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Tree<T> a = this.stack.pop();
+            for (int i = a.children.size() - 1; i >= 0; i--) {
+                if (a.children.get(i).data != null) {
+                    this.stack.addFirst(a.children.get(i));
+                }
+            }
+            this.visited.addLast(a);
+            return a.data;
+        }
+
+        /**
+         * exception when calling remove.
+         */
+        @Override
+        public void remove() throws ConcurrentModificationException {
+            throw new ConcurrentModificationException();
+        }
+    }
+}
---
+++ Task_1_2_1/src/test/java/Tests.java
@@ -0,0 +1,193 @@
+import java.util.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for the task_1_2_1.
+ */
+
+public class Tests {
+
+    @Test
+    void testAddChild() {
+        Tree<String> tree1 = new Tree<>("R1");
+        var a = tree1.addChild("A");
+        a.addChild("B");
+        Tree<String> subtree1 = new Tree<>("R2");
+        subtree1.addChild("C");
+        subtree1.addChild("D");
+        tree1.addChild(subtree1);
+
+        Tree<String> tree2 = new Tree<>("R1");
+        Tree<String> subtree2 = new Tree<>("A");
+        subtree2.addChild("B");
+        tree2.addChild(subtree2);
+        var c = tree2.addChild("R2");
+        c.addChild("C");
+        c.addChild("D");
+
+        assert (tree1.equals(tree2));
+
+        tree2.addChild("E");
+        assert (!tree1.equals(tree2));
+    }
+
+    @Test
+    void testRemove() {
+        Tree<String> tree1 = new Tree<>("R1");
+        var a = tree1.addChild("A");
+        final var b = a.addChild("B");
+        Tree<String> subtree1 = new Tree<>("R2");
+        subtree1.addChild("C");
+        subtree1.addChild("D");
+        tree1.addChild(subtree1);
+        b.remove();
+
+        Tree<String> tree2 = new Tree<>("R1");
+        Tree<String> subtree2 = new Tree<>("A");
+        final var d = subtree2.addChild("B");
+        tree2.addChild(subtree2);
+        var c = tree2.addChild("R2");
+        c.addChild("C");
+        c.addChild("D");
+        d.remove();
+
+        assert (tree1.equals(tree2));
+    }
+
+    @Test
+    void testEquals() {
+        Tree<Integer> tree1 = new Tree<>(1);
+        tree1.addChild(2);
+        var a = tree1.addChild(3);
+        a.addChild(4);
+
+        Tree<Integer> tree2 = new Tree<>(1);
+        tree2.addChild(2);
+        Tree<Integer> subtree2 = new Tree<>(3);
+        subtree2.addChild(4);
+        tree2.addChild(subtree2);
+
+        Tree<Integer> tree3 = new Tree<>(1);
+        tree3.addChild(2);
+        Tree<Integer> subtree3 = new Tree<>(3);
+        subtree3.addChild(5);
+        tree3.addChild(subtree3);
+
+        assert (tree1.equals(tree2));
+        assert (!tree1.equals(tree3));
+        assert (!tree1.equals(null));
+        assert (tree1.equals(tree1));
+    }
+
+    @Test
+    void testBfs() {
+        Tree<String> tree1 = new Tree<>("R1");
+        var a = tree1.addChild("A");
+        a.addChild("B");
+        Tree<String> subtree1 = new Tree<>("R2");
+        subtree1.addChild("C");
+        subtree1.addChild("D");
+        tree1.addChild(subtree1);
+
+        ArrayList<String> bfs = new ArrayList<String>();
+        Iterator<String> it = tree1.iterator();
+        while (it.hasNext()) {
+            var value = it.next();
+            bfs.add(value);
+        }
+        List<String> bfsRight = new ArrayList<>(Arrays.asList("R1", "A", "R2", "B", "C", "D"));
+        assertEquals(bfs, bfsRight);
+    }
+
+    @Test
+    void testBfsEmpty() {
+        Tree<String> tree = new Tree<>("R1");
+        tree.remove();
+        Iterator<String> it = tree.iterator();
+        while (it.hasNext()) {
+            fail();
+        }
+    }
+
+    @Test
+    void testDfs() {
+        Tree<String> tree1 = new Tree<>("R1");
+        var a = tree1.addChild("A");
+        a.addChild("B");
+        Tree<String> subtree1 = new Tree<>("R2");
+        subtree1.addChild("C");
+        subtree1.addChild("D");
+        tree1.addChild(subtree1);
+
+        ArrayList<String> dfs = new ArrayList<String>();
+        Iterator<String> it = tree1.dfsiterator();
+        while (it.hasNext()) {
+            var value = it.next();
+            dfs.add(value);
+        }
+        List<String> dfsRight = new ArrayList<>(Arrays.asList("R1", "A", "B", "R2", "C", "D"));
+        assertEquals(dfs, dfsRight);
+    }
+
+    @Test
+    void testDfsEmpty() {
+        Tree<String> tree = new Tree<>("R1");
+        tree.remove();
+        Iterator<String> it = tree.dfsiterator();
+        while (it.hasNext()) {
+            fail();
+        }
+    }
+
+    @Test
+    void testBfsConcurrentModificationException() {
+        Tree<String> tree1 = new Tree<>("R1");
+        var a = tree1.addChild("A");
+        a.addChild("B");
+        Tree<String> subtree1 = new Tree<>("R2");
+        subtree1.addChild("C");
+        subtree1.addChild("D");
+        tree1.addChild(subtree1);
+
+        Iterator<String> it = tree1.iterator();
+        ConcurrentModificationException thrown =
+                Assertions.assertThrows(ConcurrentModificationException.class, () -> {
+                    a.remove();
+                    while (it.hasNext()) {
+                        it.next();
+                    }
+                });
+
+        ConcurrentModificationException thrown2 =
+                Assertions.assertThrows(ConcurrentModificationException.class, () -> {
+                    it.remove();
+                });
+    }
+
+    @Test
+    void testDfsConcurrentModificationException() {
+        Tree<String> tree1 = new Tree<>("R1");
+        var a = tree1.addChild("A");
+        a.addChild("B");
+        Tree<String> subtree1 = new Tree<>("R2");
+        subtree1.addChild("C");
+        subtree1.addChild("D");
+        tree1.addChild(subtree1);
+
+        Iterator<String> it = tree1.dfsiterator();
+        ConcurrentModificationException thrown =
+                Assertions.assertThrows(ConcurrentModificationException.class, () -> {
+                    a.addChild("H");
+                    while (it.hasNext()) {
+                        it.next();
+                    }
+                });
+
+        ConcurrentModificationException thrown2 =
+                Assertions.assertThrows(ConcurrentModificationException.class, () -> {
+                    it.remove();
+                });
+    }
+}
