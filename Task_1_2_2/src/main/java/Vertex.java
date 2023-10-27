5118f5284e14295aa3ef689d35605dbd15a63e41
Anastasia Meshcheryakova <108881820+an-mrv@users.noreply.github.com>
2023-10-27 11:34:37 +0700

    Task_1_2_2 (#4)
    * Task_1_2_2
    
    * Change code style
    
    * Try 3
    
    * Added interface for working with graphs

---
+++ Task_1_2_2/src/main/java/Vertex.java
@@ -0,0 +1,26 @@
+/**
+ * Class for working with vertex objects. The vertex name is set by the user.
+ *
+ * @param <T> type of names of vertices in the graph
+ */
+public class Vertex<T> {
+    private T name;
+
+    /**
+     * Constructor.
+     *
+     * @param value the name of vertex
+     */
+    Vertex(T value) {
+        this.name = value;
+    }
+
+    /**
+     * Get the name of vertex.
+     *
+     * @return the name of vertex
+     */
+    public T getName() {
+        return this.name;
+    }
+}
