5cad3191e237b1a740cdc75812b655d15d381dea
an-mrv <an-mrv@users.noreply.github.com>
2023-11-02 11:36:17 +0000

    Deploying to gh-pages from @ an-mrv/OOP@6ea30de297e482515dd82f456486ac6d926fec61 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"SubstringFinder","l":"find(String, String)","url":"find(java.lang.String,java.lang.String)"},{"p":"<Unnamed>","c":"SubstringFinder","l":"SubstringFinder()","url":"%3Cinit%3E()"}]
