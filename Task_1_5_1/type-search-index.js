b61570be3ab42ae81a98deb85d4399a679d0f925
an-mrv <an-mrv@users.noreply.github.com>
2023-12-03 10:04:30 +0000

    Deploying to gh-pages from @ an-mrv/OOP@0f709595bb4ec2d83677a654461a63d73f258cb2 🚀

---
+++ Task_1_5_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Expression"},{"p":"<Unnamed>","l":"WrongArgumentException"},{"p":"<Unnamed>","l":"WrongPrefixNotationException"}]
