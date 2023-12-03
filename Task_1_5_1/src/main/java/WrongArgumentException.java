f5c8115ab425b0b47966a49f4e32573493e8e333
Anastasia <Nastya170204@yandex.ru>
2023-12-03 17:03:22 +0700

    Task_1_5_1

---
+++ Task_1_5_1/src/main/java/WrongArgumentException.java
@@ -0,0 +1,5 @@
+public class WrongArgumentException extends Exception{
+    public WrongArgumentException (String message) {
+        super(message);
+    }
+}
---
+++ Task_1_5_1/src/main/java/WrongPrefixNotationException.java
@@ -0,0 +1,5 @@
+public class WrongPrefixNotationException extends Exception{
+    public WrongPrefixNotationException(String message) {
+        super(message);
+    }
+}
