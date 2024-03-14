56e3222c21f331d07e8f6d55b9ee805f6366f50a
an-mrv <an-mrv@users.noreply.github.com>
2024-03-14 13:06:14 +0000

    Deploying to gh-pages from @ an-mrv/OOP@00312791b0e7aecf42906999b531bebbcd4b818e 🚀

---
+++ Task_2_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"BadClient"},{"p":"<Unnamed>","l":"Check"},{"p":"<Unnamed>","l":"Client"},{"p":"<Unnamed>","l":"Server"},{"p":"<Unnamed>","l":"Server.ServerThread"}]
