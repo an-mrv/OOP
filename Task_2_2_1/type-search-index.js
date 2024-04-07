9bd712a1ec3f337e03a21daea59cc4930e8be983
an-mrv <an-mrv@users.noreply.github.com>
2024-04-07 11:34:26 +0000

    Deploying to gh-pages from @ an-mrv/OOP@d17980db35f37464f801db2ced13ae25f4590ba4 🚀

---
+++ Task_2_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Baker"},{"p":"<Unnamed>","l":"Courier"},{"p":"<Unnamed>","l":"Order"},{"p":"<Unnamed>","l":"Pizzeria"},{"p":"<Unnamed>","l":"ReadFiles"},{"p":"<Unnamed>","l":"SynchronizedQueue"}]
